51bfc3938240a46bd780a978de3277ad26c69073
etk-23 <u2203087@rajagiri.edu.in>
2025-06-03 13:11:44 +0530

    new py testcase

---
+++ tests/test_login.py
@@ -0,0 +1,12 @@
+import re
+from playwright.sync_api import Page, expect
+
+
+def test_example(page: Page) -> None:
+    page.goto("http://127.0.0.1:5500/index.html")
+    page.get_by_role("textbox", name="Username").click()
+    page.get_by_role("textbox", name="Username").fill("admin")
+    page.get_by_role("textbox", name="Username").press("Tab")
+    page.get_by_role("textbox", name="Password").fill("1234")
+    page.get_by_role("textbox", name="Password").press("Enter")
+    page.get_by_role("button", name="Login").click()
